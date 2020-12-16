f3f01ff3a8e21eac72e77964a2ec40ce87fcf50a
Fabio Di Stasio <fabio286@gmail.com>
2020-12-16 16:32:40 +0100

    Initial commit

--- src/main/ipc-handlers/application.js
+++
@@ -1,7 +0,0 @@
-import { app, ipcMain } from 'electron';
-
-export default () => {
-   ipcMain.on('close-app', () => {
-      app.exit();
-   });
-};
--- src/renderer/i18n/supported-locales.js
+++
@@ -1,6 +0,0 @@
-export default {
-   'en-US': 'English',
-   'it-IT': 'Italiano',
-   'ar-SA': 'العربية',
-   'es-ES': 'Español'
-};
