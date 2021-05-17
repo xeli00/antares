86f3c7e8c92011333b9baa1fad465bd3331c1b25
Fabio Di Stasio <fabio286@gmail.com>
2021-05-17 23:09:27 +0200

    Improvements of download page

--- src/download.js
+++ src/download.js
@@ -10,32 +10,75 @@ document.addEventListener('DOMContentLoaded', () => {
       .then(response => response.json())
       .then(data => {
          const linux = {
-            amd64: data.assets.find(asset => /^(.*)x86_64.AppImage$/.test(asset.browser_download_url)),
-            arm64: data.assets.find(asset => /^(.*)arm64.AppImage$/.test(asset.browser_download_url)),
-            arm32: data.assets.find(asset => /^(.*)armv7l.AppImage$/.test(asset.browser_download_url))
+            amd64: {
+               data: data.assets.find(asset => /^(.*)x86_64.AppImage$/.test(asset.browser_download_url)),
+               arch: '64-bit',
+               format: 'AppImage'
+            },
+            arm64: {
+               data: data.assets.find(asset => /^(.*)arm64.AppImage$/.test(asset.browser_download_url)),
+               arch: 'ARMv8',
+               format: 'AppImage'
+            },
+            arm32: {
+               data: data.assets.find(asset => /^(.*)armv7l.AppImage$/.test(asset.browser_download_url)),
+               arch: 'ARMv7',
+               format: 'AppImage'
+            }
          };
          const windows = {
-            amd64: data.assets.find(asset => /^(.*)win.exe$/.test(asset.browser_download_url)),
-            portable: data.assets.find(asset => /^(.*)portable.exe$/.test(asset.browser_download_url))
+            amd64: {
+               data: data.assets.find(asset => /^(.*)win.exe$/.test(asset.browser_download_url)),
+               arch: '64-bit',
+               format: 'exe'
+            },
+            portable: {
+               data: data.assets.find(asset => /^(.*)portable.exe$/.test(asset.browser_download_url)),
+               arch: '64-bit',
+               format: 'portable, exe'
+            }
          };
          const mac = {
-            amd64: data.assets.find(asset => /^(.*)mac.dmg$/.test(asset.browser_download_url)),
-            arm64: data.assets.find(asset => /^(.*)arm64.dmg$/.test(asset.browser_download_url))
+            amd64: {
+               data: data.assets.find(asset => /^(.*)mac.dmg$/.test(asset.browser_download_url)),
+               arch: '64-bit',
+               format: 'dmg'
+            },
+            arm64: {
+               data: data.assets.find(asset => /^(.*)arm64.dmg$/.test(asset.browser_download_url)),
+               arch: 'ARMv8',
+               format: 'dmg'
+            }
          };
 
          for (const dist in linux) {
             if (!linux[dist]) continue;
-            linuxBlock.innerHTML += `<a class="button is-link my-2" href="${linux[dist].browser_download_url}">${linux[dist].name}</a>`;
+            linuxBlock.innerHTML += `
+               <tr>
+                  <td class="has-text-light has-text-left">${linux[dist].arch}</td>
+                  <td class="has-text-light">${linux[dist].format}</td>
+                  <td class="has-text-right"><a class="button is-primary is-small" href="${linux[dist].data.browser_download_url}" title="${linux[dist].data.name}">download</a></td>
+               </tr>`;
          }
 
          for (const dist in windows) {
             if (!windows[dist]) continue;
-            winBlock.innerHTML += `<a class="button is-link my-2" href="${windows[dist].browser_download_url}">${windows[dist].name}</a>`;
+            winBlock.innerHTML += `
+               <tr>
+                  <td class="has-text-light has-text-left">${windows[dist].arch}</td>
+                  <td class="has-text-light">${windows[dist].format}</td>
+                  <td class="has-text-right"><a class="button is-primary is-small" href="${windows[dist].data.browser_download_url}" title="${windows[dist].data.name}">download</a></td>
+               </tr>`;
          }
 
          for (const dist in mac) {
             if (!mac[dist]) continue;
-            macBlock.innerHTML += `<a class="button is-link my-2" href="${mac[dist].browser_download_url}">${mac[dist].name}</a>`;
+            macBlock.innerHTML += `
+               <tr>
+                  <td class="has-text-light has-text-left">${mac[dist].arch}</td>
+                  <td class="has-text-light">${mac[dist].format}</td>
+                  <td class="has-text-right"><a class="button is-primary is-small" href="${mac[dist].data.browser_download_url}" title="${mac[dist].data.name}">download</a></td>
+               </tr>`;
          }
       }).catch(console.log);
 });
